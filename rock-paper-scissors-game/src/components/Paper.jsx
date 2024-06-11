25386936b339a4d142db7a2d83647a8b6c5c6b72
Ray Brown <raybrown.dev@gmail.com>
2024-06-11 18:25:23 +0100

    rps game complete

--- rock-paper-scissors-game/src/components/Paper.jsx
+++
@@ -1,9 +0,0 @@
-import {FaHandPaper} from "react-icons/fa"
-
-const Paper = (props) => {
-    return (
-        <button onClick={props.choice} className='hand-icons'><FaHandPaper size={props.size}px/></button>
-    )
-}
-
-export default Paper;
--- rock-paper-scissors-game/src/components/PlayerName.jsx
+++
@@ -1,6 +0,0 @@
-const PlayerName = (props) => {
-    return <h3>Hello {props.name}, choose your weapon!</h3>
-}
-
-
-export default PlayerName;
