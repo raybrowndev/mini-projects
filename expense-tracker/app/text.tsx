c8baccc76cbc0f72503a52235d743184f05cea71
Ray Brown <raybrown.dev@gmail.com>
2024-07-04 17:48:04 +0100

    prisma database connected

---
+++ expense-tracker/app/text.tsx
@@ -0,0 +1,9 @@
+const Text = () => {
+    return ( 
+        <main>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quisquam voluptatum natus quo voluptate. Eum perspiciatis adipisci repudiandae suscipit, vero quos sint qui reiciendis! Neque eaque nisi accusamus totam hic.
+        </main>
+     );
+}
+ 
+export default Text;
